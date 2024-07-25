541dbbe78fe87f4b1d6fc0572f53e3aef331886d
deeja-ub <tomeldendjaninetara@gmail.com>
2024-07-26 04:25:11 +0800

    Halfway done

---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {images} from '../../constants';
+
+import './AboutUs.css';
+
+const AboutUs = () => (
+  <div>
+    <div className='app__aboutus app__bg flex__center section__padding' id='about'>
+      <div className='app__aboutus-overlay flex__center'>
+        <img src={images.G} alt="g letter" />
+      </div>
+      <div className='app__aboutus-content flex__center'>
+        <div className='app__aboutus-content_about'>
+          <h1 className='headtext__cormorant'>About Us</h1>
+          <img src={images.spoon} alt="about_spoon" className='spoon__img'/>
+          <p className='p__opensans'>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+          <button type='button' className='custom__button'>Know More</button>
+        </div>
+
+        <div className='app__aboutus-content_knife flex__center'>
+          <img src={images.knife} alt="knife" />
+        </div>
+
+        <div className='app__aboutus-content_history'>
+          <h1 className='headtext__cormorant'>Our History</h1>
+          <img src={images.spoon} alt="about_spoon" className='spoon__img'/>
+          <p className='p__opensans'>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+          <button type='button' className='custom__button'>Know More</button>
+        </div>
+      </div>
+
+    </div>
+  </div>
+);
+
+export default AboutUs;
